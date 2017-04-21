8538901ec8b593c41b4a444cf2567d14dcf73aa2
Peter Johnson <pete.johnsn0@gmail.com>
2017-04-20 19:31:50 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+var randomAnswers = [
+ 'It is certain',
+ 'It is decidedly so',
+ 'Without a doubt',
+ 'Yes definitely',
+ 'You may rely on it',
+ 'As I see it, yes',
+ 'Most likely',
+ 'Outlook good',
+ 'Yes',
+ 'Signs point to yes',
+ 'Reply hazy try again',
+ 'Ask again later',
+ 'Better not tell you now',
+ 'Cannot predict now',
+ 'Concentrate and ask again',
+ 'Dont count on it',
+ 'My reply is no',
+ 'My sources say no',
+ 'Outlook not so good',
+ 'Very doubtful'
+ ]
+
+function randomA() {
+  var genAnswer = randomAnswers[
+    Math.floor(Math.random() * randomAnswers.length)
+  ];
+  return genAnswer
+}
+
+function addAnswer() {
+  var ans = randomA();
+  var answerBox = document.getElementById('answerList');
+  var questionBox = document.getElementById("questionInput");
+  if ( questionBox.value === "" ) {
+    alert("You need a question to generate an answer");
+  }
+  else { addToList();
+}}
+
+function addToList() {
+var li = document.createElement('li');
+li.innerHTML = questionBox.value + ' : ' + ans;
+answerBox.appendChild(li);
+questionBox.value = ""
+}
